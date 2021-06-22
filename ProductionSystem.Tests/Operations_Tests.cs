c8249bdf31eed0315023edb6151d028cda029d6e
Tony Johnson <trj2059@gmail.com>
2021-06-21 22:19:12 -0500

    Moved production operations and tests into new folder and test file.

---
+++ ProductionSystem.Tests/Operations_Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductionSystem.ProductionOperations;
+using Xunit;
+
+namespace ProductionSystem.Tests
+{
+    public class Operations_Tests
+    {
+        private List<List<(string LHS, string RHS)>> prods { get; set; }
+
+        private StringBuilder? config { get; set; }
+
+        public Operations_Tests()
+        {
+            prods = new List<List<(string LHS, string RHS)>>();
+            prods.Add(new List<(string LHS, string RHS)>() { ("S", "Sa") });
+            prods.Add(new List<(string LHS, string RHS)>() { ("S", "aAb") });
+            prods.Add(new List<(string LHS, string RHS)>() { ("A", "aA") });
+            prods.Add(new List<(string LHS, string RHS)>() { ("A", "a") });
+            config = new StringBuilder("S");
+        }
+
+        [Fact]
+        public void Operations_Test1()
+        {
+			var result = Test1_Helper();
+			Assert.NotNull(result);
+        }
+
+        private StringBuilder? Test1_Helper()
+        {
+			try
+			{
+				if (config is StringBuilder)
+				{
+					Console.WriteLine(config.ToString());
+
+					if (Operations.canApplyProd(prods[0][0], config, 0) is bool)
+					{
+						config = Operations.applyProd(prods[0][0], config, 0);
+						if (config is StringBuilder)
+							return config;
+					}
+					else
+					{
+						throw new Exception("Cannot apply production");
+					}
+				}
+				else
+				{
+					throw new Exception("config is null");
+				}
+				return null;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return null;
+			}
+		}
+    }
+}
---
+++ ProductionSystem/ProductionOperations/Operations.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+#nullable enable
+
+
+namespace ProductionSystem.ProductionOperations
+{
+    public static class Operations
+    {
+		/// <summary>
+		/// Determines if a given production can be applied to a configuration.
+		/// </summary>
+		/// <param name="prod">A given production</param>
+		/// <param name="cfg">The current grammar configuraton</param>
+		/// <param name="index">The index of the symbol on the grammar to be substituted</param>
+		/// <returns>Returns true if the production can be applied to the configuation, false otherwise.</returns>
+		public static bool? canApplyProd((string LHS, string RHS) prod, StringBuilder cfg, int index)
+		{
+			try
+			{
+				if (index < 0)
+					throw new Exception("Index less than 0");
+				if (index > cfg.Length - 1)
+					throw new Exception("Index greater than cfg length");
+
+				Char[] cfgCharArrray = cfg.ToString().ToCharArray();
+				int i = index;
+				foreach (var c in prod.LHS)
+				{
+					if (cfgCharArrray[i] != c)
+						return false;
+					i += 1;
+				}
+
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Apply a given production to an indicated substring of a configuation
+		/// </summary>
+		/// <param name="prod">A given production</param>
+		/// <param name="cfg">The current grammar configuraton</param>
+		/// <param name="index">The index of the symbol on the grammar to be substituted</param>
+		/// <returns>
+		/// Returns the transformed configuration after applying the production, or returns null if
+		/// there was an exception
+		/// </returns>
+		public static StringBuilder? applyProd((string LHS, string RHS) prod, StringBuilder? cfg, int index)
+		{
+			try
+			{
+				if (cfg is StringBuilder)
+				{
+					cfg.Remove(index, prod.LHS.Length);
+					cfg.Insert(index, prod.RHS);
+					return cfg;
+				}
+				else
+				{
+					throw new Exception("cfg is null");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Finds the list of all indicies you can apply a given production to a cfg
+		/// Attemps to apply a production's LHS to all possible indices of a cfg
+		/// up to the cfg.Length - lhs.length.
+		/// </summary>
+		/// <param name="prod">The  production to be used in the test</param>
+		/// <param name="cfg">The configuration that is to be tested against</param>
+		/// <returns>A list of all applicable indicies the lhs can be applied to the cfg</returns>
+		public static List<int>? getListOfApplicableIndices((string LHS, string RHS) prod, StringBuilder? cfg)
+		{
+			// TODO : Finish getListOfApplicableIndices
+			try
+			{
+				var ret_value = new List<int>();
+
+				if (cfg is StringBuilder)
+				{
+					for (int i = 0; i < cfg.Length - prod.LHS.Length; i++)
+					{
+						bool? canApply = canApplyProd(prod, cfg, i);
+						if (canApply is bool)
+						{
+							if ((bool)canApply)
+								ret_value.Add(i);
+						}
+						else
+							throw new Exception("Can apply prod returned null");
+					}
+				}
+				else
+					throw new Exception("cfg is null");
+
+				return ret_value;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Determins what productions can be applied to a given configuation
+		/// </summary>
+		/// <param name="prods">A list of productions</param>
+		/// <param name="cfg">A grammar configuration</param>
+		/// <returns>All applicable productions.  Returns null if there is an exception.</returns>
+		public static List<(string LHS, string RHS, int Index)>? allApplicableProductions(List<(string LHS, string RHS)> prods, StringBuilder? cfg)
+		{
+			try
+			{
+				var ret_value = new List<(string LHS, string RHS, int Index)>();
+
+				if (cfg is StringBuilder)
+				{
+					foreach (var p in prods)
+					{
+						// UNDONE: check if production is applicable to cfg
+						List<int>? applicableIndicies = getListOfApplicableIndices(p, cfg);
+						if (applicableIndicies is List<int>)
+						{
+							foreach (var i in applicableIndicies)
+							{
+								ret_value.Add((p.LHS, p.RHS, i));
+							}
+						}
+					}
+				}
+				else
+					throw new Exception("cfg is null");
+
+				return ret_value;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+				return null;
+			}
+		}
+	}
+}
